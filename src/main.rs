c13578997bd49fe822e000d43a8178b5a9e5b9ad
David Krasnitsky <dikaveman@gmail.com>
2022-08-11 15:52:03 +0300

    Bumped Dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,31 @@
-use std::collections::HashSet;
 use arboard::Clipboard;
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashSet;
 
 fn reverse(text: &str) -> String {
-	text.chars().rev().collect()
+    text.chars().rev().collect()
 }
 
 fn reverse_brackets(text: &str) -> String {
+    let mut text = text.to_owned();
 
-	let mut text = text.to_owned();
+    text = text.replace('(', "right_bracket");
+    text = text.replace(')', "(");
+    text = text.replace("right_bracket", ")");
 
-	text = text.replace("(", "right_bracket");
-	text = text.replace(")", "(");
-	text = text.replace("right_bracket", ")");
+    text = text.replace('[', "right_bracket");
+    text = text.replace(']', "[");
+    text = text.replace("right_bracket", "]");
 
-	text = text.replace("[", "right_bracket");
-	text = text.replace("]", "[");
-	text = text.replace("right_bracket", "]");
+    text = text.replace('{', "right_bracket");
+    text = text.replace('}', "{");
+    text = text.replace("right_bracket", "}");
 
-	text = text.replace("{", "right_bracket");
-	text = text.replace("}", "{");
-	text = text.replace("right_bracket", "}");
-	
-	text
-	
+    text
 }
 
 fn extract_numbers(s: &str) -> Option<HashSet<&str>> {
-
     // `lazy_static!` to run once and prevent loops from needlessly repeating this section
     lazy_static! {
         static ref RE: Regex = Regex::new(r#"\d+"#).unwrap();
@@ -43,48 +40,39 @@ fn extract_numbers(s: &str) -> Option<HashSet<&str>> {
     Some(numbers_set)
 }
 
-
 fn reverse_numbers(text: &str) -> Option<String> {
+    // Detect numbers, reverse them and replace them
+    match extract_numbers(text) {
+        Some(numbers_set) => {
+            let mut text = text.to_owned();
 
-	// Detect numbers, reverse them and replace them
-	match extract_numbers(text) {
+            for number in numbers_set {
+                text = text.replace(number, &reverse(number));
+            }
 
-		Some(numbers_set) => {
-
-			let mut text = text.to_owned();
-
-			for number in numbers_set {
-				text = text.replace(number, &reverse(number));
-			}
-
-			Some(text)
-		},
-		_ => None
-	}
-
+            Some(text)
+        }
+        _ => None,
+    }
 }
 
 fn reverse_clipboard(text: &str) -> String {
-
-	text.lines()
-	.map(|line| 
-			if let Some(reversed_numbers_line) = reverse_numbers(line) {
-				format!("{}\r\n", &reverse(&reversed_numbers_line))
-			} else {
-				format!("{}\r\n", &reverse(line))
-			}
-	)
-	.map(|line| reverse_brackets(&line))
-	.collect()
-	
+    text.lines()
+        .map(|line| {
+            if let Some(reversed_numbers_line) = reverse_numbers(line) {
+                format!("{}\r\n", &reverse(&reversed_numbers_line))
+            } else {
+                format!("{}\r\n", &reverse(line))
+            }
+        })
+        .map(|line| reverse_brackets(&line))
+        .collect()
 }
 
 fn main() {
+    let mut clipboard = Clipboard::new().unwrap();
 
-	let mut clipboard = Clipboard::new().unwrap();
-	
-	let content = clipboard.get_text().unwrap();
-	
-	clipboard.set_text(reverse_clipboard(&content)).unwrap();
+    let content = clipboard.get_text().unwrap();
 
+    clipboard.set_text(reverse_clipboard(&content)).unwrap();
 }
